56b7285a210a3d35002575d0f0bd28862a6762fb
Junghwan Park <reserve.dev@gmail.com>
2018-07-20 21:59:00 +0900

    use bulkCreate for insertion

--- controllers/dict/util.js
+++ controllers/dict/util.js
@@ -1,28 +1,33 @@
 "use strict";
 
+const util = require("util");
 const lineReader = require("line-reader");
 const model = require("../../models");
 
 exports.importDict = function(filename) {
+  var items = [];
+
   try {
-    lineReader.eachLine(filename, function(line) {
+    var eachLine = util.promisify(lineReader.eachLine);
+
+    eachLine(filename, function(line) {
       var elements = line.split("\t");
 
       var token = elements[0];
       var attrs = elements.slice(1, elements.length);
 
-      var result = [];
-
       attrs.forEach(attr => {
-        attr = attr.split(":");
+        var attr = attr.split(":");
         var pos = attr[0];
         var tf = attr[1];
 
-        // result.push({ token, pos, tf });
-        model.Dict.create({ token, pos, tf });
+        items.push({ token, pos, tf });
       });
-
-      // return result;
+    }).then(() => {
+      console.log("read file - done : " + items.length + " items");
+      model.Dict.bulkCreate(items, { logging: false }).then(() => {
+        console.log("insert items - done");
+      });
     });
   } catch (err) {
     throw err;
